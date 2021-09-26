be7252e73efb102d0e039bf825f517cd221faab5
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2021-09-26 13:44:47 +0200

    Login , Register ,Forget Password Page Setup

--- resources/js/components/auth/forget.vue
+++ resources/js/components/auth/forget.vue
@@ -11,35 +11,25 @@
               <div class="col-lg-12">
                 <div class="login-form">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Forget Password</h1>
                   </div>
     <form class="user">
     <div class="form-group">
         <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp"
-        placeholder="Enter Email Address">
+        placeholder="Enter Existing Email Address">
     </div>
+
     <div class="form-group">
-        <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password">
-    </div>
-    <div class="form-group">
-        <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
-        <input type="checkbox" class="custom-control-input" id="customCheck">
-        <label class="custom-control-label" for="customCheck">Remember
-            Me</label>
-        </div>
-    </div>
-    <div class="form-group">
-        <a href="index.html" class="btn btn-primary btn-block">Login</a>
+        <a href="index.html" class="btn btn-primary btn-block">Forget Password</a>
     </div>
 
 
     </form>
                   <hr>
                   <div class="text-center">
-                  <router-link to="/register" class="font-weight-bold small">Create an Account!</router-link>
+                  <router-link to="/" class="font-weight-bold small">Back To Login!</router-link>
                   </div>
                   <div class="text-center">
-                  <router-link to="/forget" class="font-weight-bold small">Forget password</router-link>
                   </div>
                 </div>
               </div>
