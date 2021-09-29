c548f0f8f389319c9b6e1118a27183ceb9e05837
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2021-09-29 17:24:49 +0200

    secure register page

--- resources/js/components/auth/register.vue
+++ resources/js/components/auth/register.vue
@@ -1,7 +1,9 @@
+
+
 <template>
 
-<div>
- <div class="row justify-content-center">
+	<div>
+		<div class="row justify-content-center">
       <div class="col-xl-10 col-lg-12 col-md-9">
         <div class="card shadow-sm my-5">
           <div class="card-body p-0">
@@ -11,36 +13,38 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Register</h1>
                   </div>
-        <form>
+      <form class="user" @submit.prevent="register">
         <div class="form-group">
 
-            <input type="text" class="form-control" id="exampleInputFirstName" placeholder="Enter Your Full Name">
+          <input type="text" class="form-control" id="exampleInputFirstName" placeholder="Enter Your Full Name" v-model="form.name" required="">
         </div>
+
         <div class="form-group">
 
-            <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp"
-            placeholder="Enter Email Address">
-        </div>
-        <div class="form-group">
-
-            <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password">
+          <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp"
+            placeholder="Enter Email Address" v-model="form.email" required="">
         </div>
         <div class="form-group">
 
-            <input type="password" class="form-control" id="exampleInputPasswordRepeat"
-            placeholder="Confirm Password">
+          <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password" v-model="form.password" required="">
+        </div>
+
+        <div class="form-group">
+
+          <input type="password" class="form-control" id="exampleInputPasswordRepeat"
+            placeholder="Confirm Password" v-model="form.password_confirmation" required="">
         </div>
         <div class="form-group">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
+          <button type="submit" class="btn btn-primary btn-block">Register</button>
         </div>
-        </form>
+
+      </form>
                   <hr>
                   <div class="text-center">
 
-                       <router-link to="/" class="font-weight-bold small">Already have an account?</router-link>
+  <router-link to="/" class="font-weight-bold small">Already have an account?</router-link>
                   </div>
                   <div class="text-center">
-
                   </div>
                 </div>
               </div>
@@ -49,16 +53,57 @@
         </div>
       </div>
     </div>
-</div>
+	</div>
+
 
 </template>
 
 
 
+
 <script type="text/javascript">
 
-</script>
+export default {
+    created(){
+        if(User.loggedIn()){
+            this.$router.push({name : 'home'})
+        }
+    },
 
+    data(){
+    return{
+        form : {
+            name : null ,
+            email : null ,
+            password : null,
+            confirm_password : null
+        },
+        // errors:{}
+    }
+},
+methods : {
+    register(){
+        axios.post('api/auth/register', this.form)
+       .then(res => {
+           User.responseAfterLogin(res)
+         Toast.fire({
+               icon: 'success',
+               title: 'Signed in successfully'
+           })
+           this.$router.push({name : 'home'})      // make redirection to home after login
+           })
+
+        .catch(error =>this.errors =  error.response.data.errors)   // to display error
+
+
+
+    }
+}
+
+}
+
+
+</script>
 
 
 <style type="text/css">
